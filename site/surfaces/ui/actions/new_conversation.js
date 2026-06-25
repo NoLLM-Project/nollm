b5ae9c409308429b7181732076d1f0ad72d3bb33
NoLLM-Project <nollm-keeper@proton.me>
2026-06-25 11:26:14 -0400

    Add files via upload

--- site/surfaces/ui/actions/new_conversation.js
+++ site/surfaces/ui/actions/new_conversation.js
@@ -1,6 +1,6 @@
 // surfaces/ui/actions/new_conversation.js
 
-import { newConversationId } from "../state/conversation_id.js";
+import { newConversationId as generateNewConversationId } from "../state/conversation_id.js";
 
 export function actionNewConversation() {
   newConversationId();
